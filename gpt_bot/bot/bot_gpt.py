4419f5c83f2491ecb42b423d4517336dba4f8aa0
GriMary5566 <grimar_nsk@mail.ru>
2023-04-01 19:13:07 +0300

    Исправлен подсчет токенов в сообщении. В .env вынесен SEKRET_KEY. Переименована функция question_save в message_save.

--- gpt_bot/bot/bot_gpt.py
+++ gpt_bot/bot/bot_gpt.py
@@ -6,8 +6,8 @@ from bot.models import BotMessage
 
 
 SYSTEM_MESSAGE = {
-    "role": "system",
-    "content": "You are a helpful assistant."
+    'role': 'system',
+    'content': 'You are a helpful assistant.'
 }
 ENCODING_MODEL = 'gpt-3.5-turbo'
 
@@ -18,7 +18,7 @@ def num_tokens_from_message(string: str, role: str, encoding_model: str) -> int:
     return num_tokens
 
 @sync_to_async
-def qustion_save(chat, role, content, content_tokens):
+def messge_save(chat, role, content, content_tokens):
     """Сохраняет сообщение в базу данных и возвращает его."""
     question = BotMessage.objects.create(
         chat_id=chat.id,
@@ -39,17 +39,16 @@ def get_queryset(chat):
 def get_pre_messages(queryset, question):
     """Возвращает запрос к Chat API и количество токенов этого запроса."""
     last_message = {
-        "role": question.role,
-        "content": question.content
+        'role': question.role,
+        'content': question.content
     }
     pre_messages = [SYSTEM_MESSAGE]
     system_message_tokens = num_tokens_from_message(
-        SYSTEM_MESSAGE["content"],
-        SYSTEM_MESSAGE["role"],
+        SYSTEM_MESSAGE['content'],
+        SYSTEM_MESSAGE['role'],
         ENCODING_MODEL
     )
     sum_tokens = system_message_tokens + 4
-    print(sum_tokens)
     print(queryset)
     if len(queryset) == 0:
         sum_tokens += question.content_tokens
@@ -59,10 +58,10 @@ def get_pre_messages(queryset, question):
     for query in queryset:        
         if sum_tokens + query['content_tokens'] >= 2000:
             return pre_messages, sum_tokens
-        sum_tokens += query['content_tokens'] + 4
+        sum_tokens += query['content_tokens']
         pre_message = {}
-        pre_message["role"] = query["role"]
-        pre_message["content"] = query["content"]
+        pre_message['role'] = query['role']
+        pre_message['content'] = query['content']
         pre_messages.insert(1, pre_message)
     
     print(pre_messages)
@@ -77,7 +76,7 @@ async def echo(update, context):
     content = update.effective_message.text
     content_tokens = num_tokens_from_message(content, role, ENCODING_MODEL)
     queryset = await get_queryset(chat)
-    question = await qustion_save(chat, role, content, content_tokens )
+    question = await messge_save(chat, role, content, content_tokens )
     pre_messages, sum_tokens = await get_pre_messages(queryset, question)
 
     await update.message.reply_text(f'Тестируем бота. Количество токенов в строке: {content_tokens}')
